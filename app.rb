d56385e1f00287ed6dbe63ee0b661a1c1d0562bc
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-05 12:44:43 +0100

    test(feature): write first test and configure rspec to work with capybara

--- app.rb
+++ app.rb
@@ -2,9 +2,15 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 class App < Sinatra::Base
-  set :sessions, true
+  
+  configure :development do
+    register Sinatra::Reloader
+  end
 
   get '/' do
     'Hello World!'
   end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
