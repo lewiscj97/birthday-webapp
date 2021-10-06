8efbda63b52fa9ae4c6f3bc7cb856d153a9755a4
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-06 10:22:26 +0100

    test(person): add tests for other attributes

--- spec/lib/person_spec.rb
+++ spec/lib/person_spec.rb
@@ -10,4 +10,16 @@ describe Person do
       expect(@person.name).to eq "Lewis"
     end
   end
+
+  describe '#day' do
+    it 'returns the day' do
+      expect(@person.day).to eq 18
+    end
+  end
+
+  describe '#month' do
+    it 'returns the month' do
+      expect(@person.month).to eq "March"
+    end
+  end
 end
