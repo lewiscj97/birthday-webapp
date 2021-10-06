f6f4b6335f3facfbd02b2a39957180eda94b935e
Lewis Jones <lewis.jones1997@protonmail.com>
2021-10-06 11:18:07 +0100

    person

--- lib/person.rb
+++ lib/person.rb
@@ -5,16 +5,25 @@ class Person
 
   def initialize(name, day, month)
     @name = name
-    @day = day
+    @day = day.to_i
     @month = month
+    @birthday = Date.new(today.year, month_to_integer, @day) 
   end
 
   def birthday_today?
-    today = Date.today
-    result = today.mon == month_to_integer && today.mday == @day.to_i
+    result = today.mon == month_to_integer && today.mday == @day
+  end
+
+  def days_until_birthday
+    # if birthday has already been, add another year to the birthday
+    (@birthday < today) ? (@birthday.next_year - today).to_i : (@birthday - today).to_i
   end
 
   private
+
+  def today
+    Date.today
+  end
 
   def month_to_integer
     Date::MONTHNAMES.index(@month)
